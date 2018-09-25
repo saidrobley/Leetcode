6b139d7a4f8f96d1696041576c7d0ffddaedc679
Said Robley <saidrobley@gmail.com>
2018-09-25 14:24:00 -0600

    initial commit

---
+++ ReverseInteger.java
@@ -0,0 +1,25 @@
+
+
+public class ReverseInteger {
+	
+	public static void main(String[] args) {
+		System.out.println(reverse(120));
+	}
+	public static int reverse(int x) {
+		boolean negative = false;
+		if(x < 0) {
+			negative = true;
+			x *= -1;
+		}
+		long reverse = 0;
+		while (x > 0) {
+			reverse = (reverse * 10) + (x % 10);
+			x /= 10;
+		}
+		if(reverse > Integer.MAX_VALUE){
+			return 0;
+		}
+		return negative ? (int)(-1 * reverse) : (int) reverse;
+	}
+	
+}
