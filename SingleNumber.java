b2939cbc3e6c8dcb6ecbb08f5e42ade6fc7fec25
Said Robley <saidrobley@gmail.com>
2018-09-27 10:36:15 -0600

    find the single element that does not appear twice

---
+++ SingleNumber.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+/*
+	Given a non-empty array of integers, every element appears twice
+	except for one. Find that single one.
+*/
+
+public class SingleNumber {
+	
+	public static void main(String[] args) {
+		int[] arr = {4, 1, 2, 1, 2};
+		System.out.println(getSingleNumber(arr));
+	}
+	
+	public static int getSingleNumber(int[] arr) {
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int number: arr) {
+			if(stack.isEmpty()){
+				stack.push(number);
+			} else if(stack.peek() == number) {
+					stack.pop();
+				
+			}
+		}
+			
+		return stack.pop();
+	}
+	
+}
